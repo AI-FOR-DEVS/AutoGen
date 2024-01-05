e03941712c808f910dab4fb691d6de46fb9fc57c
Sebastian <sebastian@prepend.de>
2024-01-05 08:42:04 +0100

    Add Flask & Frontend

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
 from autogen import AssistantAgent, UserProxyAgent, GroupChat, GroupChatManager, config_list_from_json
 from search import search, search_declaration
+from flask import Flask, jsonify, request, render_template
+
+app = Flask(__name__)
 
 assistant = AssistantAgent('assistant', llm_config={"functions": [search_declaration], "config_list": config_list_from_json("OAI_CONFIG_LIST")})
 
-user_proxy = UserProxyAgent('user_proxy', function_map={"Search": search})
+def is_termination_msg(data):
+    has_content = "content" in data and data["content"] is not None
+    return has_content and "TERMINATE" in data["content"]
+
+user_proxy = UserProxyAgent('user_proxy', is_termination_msg=is_termination_msg, human_input_mode='NEVER', function_map={"Search": search})
 
 llm_config = {"config_list": config_list_from_json("OAI_CONFIG_LIST")}
 
@@ -13,4 +20,16 @@ groupchat = GroupChat(agents=[assistant, cynic, user_proxy], messages=[])
 
 groupchat_manager = GroupChatManager(groupchat, llm_config=llm_config)
 
-user_proxy.initiate_chat(groupchat_manager, message="Find the latest stock price of Apple")
+@app.route('/run')
+def run():
+    stockname = request.args.get('stock')
+    user_proxy.initiate_chat(groupchat_manager, message=f"Find the latest stock price of {stockname}")
+
+    messages = user_proxy.chat_messages[groupchat_manager]
+    return jsonify(messages)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+app.run(debug=True, port=8080)
